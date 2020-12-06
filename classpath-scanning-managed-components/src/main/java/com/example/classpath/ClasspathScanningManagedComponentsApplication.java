ee1f19a7d773a5fb161a09f58d7674345033d4e1
in-a-day <997582500@qq.com>
2020-12-06 23:47:51 +0800

    components

---
+++ classpath-scanning-managed-components/src/main/java/com/example/classpath/ClasspathScanningManagedComponentsApplication.java
@@ -0,0 +1,14 @@
+package com.example.classpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class ClasspathScanningManagedComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasspathScanningManagedComponentsApplication.class, args);
+	}
+
+}
